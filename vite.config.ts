476786b2027d613b711f693dbbba96622b4948e4
Semen Nazarenko <semen.nazarenko@axenix.pro>
2025-08-09 21:43:06 +0300

    fix:config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import {defineConfig} from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/gorenije-show/" : "/",
+  base: "/gorenije-show/",
   plugins: [react(), tailwindcss()],
 });
